eb25321b41d794ddab82cba3df6e32cfe2386b6a
josephyzz <miguelneto275@gmail.com>
2026-05-23 20:27:06 -0300

    Day 24: I learned about using files.

---
+++ Intermediate/day24/main.py
@@ -0,0 +1,9 @@
+with open('my_file.txt') as file:
+    content = file.read()
+    print(content)
+
+with open('new_file.txt', mode='a') as file:
+    file.write('New Text.\n')
+
+with open('my_file.txt', mode='a') as file:
+    file.write('New Text.\n')
