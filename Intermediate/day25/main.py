1106b26be3bc3c3e92053b53ff539cb116953273
josephyzz <miguelneto275@gmail.com>
2026-06-02 22:32:19 -0300

    Day 25: U.S State Game Project

--- Intermediate/day25/main.py
+++ Intermediate/day25/main.py
@@ -13,25 +13,48 @@
 #    print(temperatures)
 
 
-import pandas
+# import pandas
 
-data = pandas.read_csv('weather_data.csv')
+# data = pandas.read_csv('weather_data.csv')
 
-data_dict = data.to_dict()
-print(data_dict)
-
-temp_list = data['temp'].to_list()
-print(temp_list)
-
-# Average
-print(int(data['temp'].mean()))
-
-# Max Number to temp column
-print(data['temp'].max())
-
-# Get Data in Columns
-print(data['condition'])
-print(data.condition)
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data['temp'].to_list()
+# print(temp_list)
+#
+# # Average
+# print(int(data['temp'].mean()))
+#
+# # Max Number to temp column
+# print(data['temp'].max())
+#
+# # Get Data in Columns
+# print(data['condition'])
+# print(data.condition)
 
 # Get Data in Row
-print(data[data.day == 'Monday'])
+# print(data[data.day == 'Monday'])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == 'Monday']
+# monday_temp = monday.temp[0]
+# monday_temp_F = monday_temp * 9 / 5 + 32
+
+# print('Fahrenheit Temp: %s °F' % (monday_temp_F))
+
+# Create a dataframe form scratch
+# data_dict = {'students': ['Amy', 'James', 'Angela'], 'scores': [76, 56, 65]}
+# data = pandas.DataFrame(data_dict)
+# data.to_csv('new_data.csv')
+
+import pandas
+
+data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+
+result = {'Fur Color': ['Gray', 'Cinnamon', 'Black'], 'Count': []}
+for i in result['Fur Color']:
+    result['Count'].append(len(data[data['Primary Fur Color'] == i]))
+
+df = pandas.DataFrame(result)
+df.to_csv('squirrel_count.csv')
